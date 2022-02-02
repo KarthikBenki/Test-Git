c8e24d4f5987a80db2fbf92392e913d3311f906c
karthikbenki <karthikmc007@gmail.com>
2022-02-02 09:18:24 +0530

    Added : Welcome to Git messege

---
+++ src/com/blz/git/GitRunner.java
@@ -0,0 +1,11 @@
+package com.blz.git;
+
+public class GitRunner {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to GitHub");
+		// TODO Auto-generated method stub
+
+	}
+
+}
