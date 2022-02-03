223dfc070f2d32b5005a42ec3696bfbf732c75fc
karthikbenki <karthikmc007@gmail.com>
2022-02-03 08:51:45 +0530

    added:subtraction of two variables

---
+++ src/com/blz/git/Substraction.java
@@ -0,0 +1,11 @@
+package com.blz.git;
+
+public class Substraction {
+	public static void main(String[] args) {
+		int num1=30;
+		int num2=20;
+		int sub = num1-num2;
+		System.out.println("The Difference is "+sub);
+	}
+
+}
