73bdfba4c3e03816833ffb2a5c32608e2531ebfe
KARTHIK M C <karthikmc007@gmail.com>
2022-02-03 07:10:58 +0530

    modified Git runner

--- src/com/blz/git/GitRunner.java
+++ src/com/blz/git/GitRunner.java
@@ -1,10 +1,170 @@
 package com.blz.git;
-
+import java.util.Random;
+import java.util.Scanner;
 public class GitRunner {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to GitHub");
 		// TODO Auto-generated method stub
+		//variables
+		//datatypes
+		//types
+		//how to accept inpit from user
+//		how to print??
+		//decition making if ,if else ,switch
+		//looping for, while loop do while loop
+		//oop's classes ,object method overriding , over riding in heritence
+		//java 8 features like lambda, stream API..
+		//MYsql ,Regex , Exception handling
+		//Web Intro HTML Javascript
+		//assignment submissions, interaction in sessions and Workshops performance
+		//variable: name given to memory location
+		
+//		mobile number = 9786574530; //
+		
+//		datatype variablename = value;
+//		datatype is size and type of value is allowed 
+		/*
+		 * primitive and non primitive datatypes
+		 *  
+		 * char : character
+		 * 
+		 * byte : 1 byte of memory
+		 * int : 4 bytes
+		 * short : 2 bytes
+		 * long : 8 bytes
+		 * 
+		 * float : 4 bytes
+		 * double : 8 bytes
+		 * 
+		 * boolean : 1 bit | 1 or 0
+		 * 
+		 */
+		
+			Scanner sc = new Scanner(System.in);
+			
+//			char ch = 'A';
+//			System.out.println(ch);
+//			boolean isAdult = true;// >=18 years
+//			double salary = 56045.87;
+//			
+//			float age = 19.2f;
+//			
+//			long mobile = 8765432567l;
+//			
+//			char alpha = 'a';
+//			
+//			int noOfEmployee = 250;
+//			
+//			System.out.println("isAdult "+isAdult );
+//			System.out.println("salary "+salary );
+//			System.out.println("mobile "+mobile );
+//			System.out.println("alpha "+alpha );
+//			System.out.println("noOfEmployee "+noOfEmployee );
+//			System.out.println("age "+age );
+			
+//			System.out.println("Enter your age");
+//			int age = sc.nextInt();
+//			
+//			if(age>=18)
+//			{
+//				System.out.println("hey you can vote\n");
+//			}
+//			else if(age<18 && age>0)
+//			{
+//				System.out.println("you cant vote\n");
+//			}
+//			else
+//			{
+//				System.out.println("never born");
+//			}
+			
+//			int number = sc.nextInt();
+//			
+//			if(number>0)
+//			{
+//				System.out.println("the number is possitive");
+//			}
+//			else if (number<0)
+//			{
+//				System.out.println("The number is negative");
+//			}
+//			else
+//			{
+//				System.out.println("zero");
+//			}
+			
+//			int days = sc.nextInt();
+//					switch(days)
+//					{
+//					case 0:
+//						System.out.println("Sunday!");
+//						break;
+//					case 1:
+//						System.out.println("Monday!");
+//						break;
+//					case 2:
+//						System.out.println("tuesday!");
+//						break;
+//					case 3:
+//						System.out.println("Wednesday!");
+//						break;
+//					case 4:
+//						System.out.println("Thrusday!");
+//						break;
+//					case 5:
+//						System.out.println("Friday!");
+//						break;
+//					case 6:
+//						System.out.println("Saturday!");
+//						break;
+//					default:
+//						System.out.println("Invalid days");
+//						break;
+//					}
+		
+//			Random ran = new Random();
+//			int dice = ran.nextInt(6)+1;
+//			
+//			switch(dice)
+//			{
+//			case 1:
+//				System.out.println("one");
+//				break;
+//				
+//			case 2:
+//				System.out.println("Two");
+//				break;
+//				
+//			case 3:
+//				System.out.println("Three");
+//				break;
+//				
+//			case 4:
+//				System.out.println("Four");
+//				break;
+//				
+//			case 5:
+//				System.out.println("Five");
+//				break;
+//				
+//			case 6:
+//				System.out.println("Six");
+//				break;
+//				
+//			default:
+//				System.out.println("Invalid");
+//				break;					
+//			}
+			
+//			if(ran.nextInt(2) == 1)
+//			{
+//				System.out.println("heads");
+//			}
+//			else
+//			{
+//				System.out.println("Tails");
+//			}	
 
 	}
 
