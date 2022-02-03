3a9ad988409d972a4ea0ef7f3fa779c58bf4915a
karthikbenki <karthikmc007@gmail.com>
2022-02-03 08:56:38 +0530

    modified: variable name change

--- src/com/blz/git/Substraction.java
+++ src/com/blz/git/Substraction.java
@@ -2,9 +2,9 @@ package com.blz.git;
 
 public class Substraction {
 	public static void main(String[] args) {
-		int num1=30;
-		int num2=20;
-		int sub = num1-num2;
+		int num1_1 = 30;
+		int num2_2 = 20;
+		int sub = num1_1-num2_2;
 		System.out.println("The Difference is "+sub);
 	}
 
