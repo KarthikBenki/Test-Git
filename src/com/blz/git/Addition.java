51cac6a8bb365503672c5c4712ec79756657160b
karthikbenki <karthikmc007@gmail.com>
2022-02-03 08:38:17 +0530

    Added Addition.java

---
+++ src/com/blz/git/Addition.java
@@ -0,0 +1,14 @@
+package com.blz.git;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int addition = num1 + num2;
+		System.out.println("The addition is "+addition);
+		// TODO Auto-generated method stub
+
+	}
+
+}
